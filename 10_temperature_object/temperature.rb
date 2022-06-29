cbf00c641368458e3f4be34da284c2ef609c3a07
pdany1116 <popa.daniel@agilefreaks.com>
2022-06-29 17:42:38 +0300

    Create Temperature class with C/F helpers

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../01_temperature/temperature'
+
+class Temperature
+  def initialize(options = {})
+    @options = options
+  end
+
+  def in_fahrenheit
+    if @options.key?(:c)
+      ctof(@options[:c])
+    else
+      @options[:f]
+    end
+  end
+
+  def in_celsius
+    if @options.key?(:f)
+      ftoc(@options[:f])
+    else
+      @options[:c]
+    end
+  end
+end
