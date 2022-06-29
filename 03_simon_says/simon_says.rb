395045551c77165b2a2b7fe3017367c60ac9adf5
pdany1116 <popa.daniel@agilefreaks.com>
2022-06-29 15:18:35 +0300

    Implement titleize function

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+SMALL_WORDS = %w[and the over a an in of]
+
 def echo(str)
   str
 end
@@ -24,4 +26,16 @@ end
 
 def first_word(str)
   str.split[0]
+end
+
+def titleize(str)
+  words = str.capitalize.split
+
+  words.each do |word|
+    next if SMALL_WORDS.include?(word)
+
+    word.capitalize!
+  end
+
+  words.join(' ')
 end
