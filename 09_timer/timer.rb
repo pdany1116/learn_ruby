de41eb5587620368924a4455b607c2fe9f309f45
pdany1116 <popa.daniel@agilefreaks.com>
2022-06-29 17:30:13 +0300

    Implement Timer#time_string

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -9,8 +9,18 @@ class Timer
     @seconds = 0
   end
 
+  def time_string
+    seconds = @seconds
+    hours = seconds / 3600
+    seconds = seconds % 3600
+    minutes = seconds / 60
+    seconds = seconds % 60
+
+    "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  end
+
   def padded(value)
-    return "0#{value}" if value / 10 == 0
+    return "0#{value}" if (value / 10).zero?
 
     value.to_s
   end
