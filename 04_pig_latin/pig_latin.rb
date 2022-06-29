61f52a2792182314aa4080805c18ef3374e2a4f4
pdany1116 <popa.daniel@agilefreaks.com>
2022-06-29 15:58:21 +0300

    Implement pig latin translate

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+PHONEMES = %w[qu]
+
+def translate(str)
+  str.split.map! { |word| translate_word(word) }.join(' ')
+end
+
+def translate_word(word)
+  while starts_with_consonant?(word)
+    phoneme = extract_phoneme(word)
+
+    if phoneme.empty?
+      word += word[0]
+      word = word[1..-1]
+    else
+      word += phoneme
+      word.slice!(phoneme)
+    end
+  end
+
+  "#{word}ay"
+end
+
+def starts_with_consonant?(word)
+  word =~ /^[^aeiou]/
+end
+
+def extract_phoneme(word)
+  PHONEMES.each do |phoneme|
+    return phoneme if word =~ /^#{phoneme}/
+  end
+
+  ''
+end
